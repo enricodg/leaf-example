cf2982deb89cc0d66fdc18fbd960e4a1b196ad13
enrico <enrico.daniswara@tanihub.com>
2022-04-25 10:37:33 +0700

    remove unused file

--- internal/usecases/forwarder/forwardMessage.go
+++
@@ -1,7 +0,0 @@
-package forwarder
-
-import "context"
-
-func (u *ucForwardMessage) ForwardMessage(ctx context.Context, message string) error {
-	return u.Outbound.Messaging.Hello.PublishMessage(ctx, message)
-}
